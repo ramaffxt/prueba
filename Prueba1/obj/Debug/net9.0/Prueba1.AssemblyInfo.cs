af65f7ab42aa385aaa772458b9df7a1874bcd489
ramaffxt <ramiro.perez@correo.ucu.edu.uy>
2025-06-23 20:32:15 -0300

    Cambio en gitignore

--- Prueba1/obj/Debug/net9.0/Prueba1.AssemblyInfo.cs
+++ Prueba1/obj/Debug/net9.0/Prueba1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70ce31886fb5339bb62d53c2d25ef86670cad8b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
